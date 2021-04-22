ca9e8f4059a872fbc8efd0b071eea5723215b86b
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-04-22 12:06:04 +0200

    tilføjelse af sider

---
+++ loud-theme/page-sendeplan.php
@@ -0,0 +1,35 @@
+<div id="sendeplanside">
+    <?php
+/**
+ * The template for displaying all single posts and attachments.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+
+get_header();
+
+do_action( 'hestia_before_single_page_wrapper' );
+
+?>
+    <div class="<?php echo hestia_layout(); ?>">
+        <?php
+	$class_to_add = '';
+	if ( class_exists( 'WooCommerce', false ) && ! is_cart() ) {
+		$class_to_add = 'blog-post-wrapper';
+	}
+	?>
+        <div class="blog-post <?php esc_attr( $class_to_add ); ?>">
+            <div class="container">
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+        <?php get_footer(); ?>
+    </div>
