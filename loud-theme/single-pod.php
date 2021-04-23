34532a390561609774be023cdfd838e03c91e1bb
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-04-23 10:03:53 +0200

    episoder tilføjet

--- loud-theme/single-pod.php
+++ loud-theme/single-pod.php
@@ -24,16 +24,43 @@ do_action( 'hestia_before_single_post_wrapper' );
                             <p class="langtekst"></p>
                         </div>
                     </article>
+
+                    <section id="episoder">
+                        <template>
+                            <article>
+                                <img src="" alt="" class="episodebillede">
+                                <div>
+                                    <h2 class="episodenavn"></h2>
+                                    <h3 class="episodenr"></h3>
+                                    <p class="om_episoden"></p>
+                                </div>
+                            </article>
+                        </template>
+                    </section>
                 </main>
+
+
+
+
                 <script>
                     let pod;
-                    const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/pod/" + <?php echo get_the_ID() ?>;
+                    let episoder;
+                    let aktuelpodcast = <?php echo get_the_ID() ?>;
+                    const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/pod/" + aktuelpodcast;
+                    const episodeUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/epi?per_page=100";
+
+                    const container = document.querySelector("#episoder");
 
                     async function getJSON() {
                         const data = await fetch(dbUrl);
                         pod = await data.json();
 
+                        const data2 = await fetch(dbUrl);
+                        episoder = await data2.json();
+                        console.log("episoder:", episoder);
+
                         visPod();
+                        visEpisodere();
                     }
 
                     function visPod() {
@@ -42,6 +69,32 @@ do_action( 'hestia_before_single_post_wrapper' );
                         document.querySelector(".billede").src = pod.billede.guid;
                         document.querySelector(".langtekst").textContent = pod.langtekst;
                     }
+
+                    function visEpisodere() {
+                        console.log("visEpisodere");
+                        let temp = document.querySelector("template");
+                        episoder.forEach(epi => {
+                            console.log("loop id:", aktuelpodcast);
+                            if (epi.horer_til_podcast == aktuelpodcast) {
+                                console.log("loop kører id:", aktuelpodcast);
+                                let klon = temp.cloneNode(true).content;
+
+
+                                klon.querySelector(".episodebillede").src = epi.billede.guid;
+                                klon.querySelector(".episodenavn").textContent = epi.title.rendered;
+                                klon.querySelector(".episodenr").innerHTML = epi.episodenr;
+                                klon.querySelector(".om_episoden").innerHTML = epi.om_episoden;
+                                klon.querySelector("article").addEventListener("click", () => {
+                                    location.href = epi.link;
+                                })
+
+                                klon.querySelector("article").href = epi.link;
+                                console.log("epi", epi.link);
+                                container.appendChild(klon);
+                            }
+                        })
+                    }
+
                     getJSON();
 
                 </script>
