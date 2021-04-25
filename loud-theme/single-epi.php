1ccc419326c1c71eb90c700a97b1b2dbb51e274e
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-04-26 00:24:56 +0200

    oprettelse af single view af en enkelt episode

---
+++ loud-theme/single-epi.php
@@ -0,0 +1,75 @@
+<div id="enkelepisode">
+    <?php
+/**
+ * The template for displaying all single posts and attachments.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+
+get_header();
+
+do_action( 'hestia_before_single_post_wrapper' );
+?>
+
+    <div class="<?php echo hestia_layout(); ?>">
+        <div class="blog-post blog-post-wrapper">
+            <div class="container">
+
+                <section id="primary" class="content-area">
+                    <main id="main" class="site-main">
+                        <article>
+                            <img src="" alt="" class="billede">
+                            <div>
+                                <h2 class="episodenavn"></h2>
+                                <h3 class="episodenr"></h3>
+                                <p class="om_episoden"></p>
+                            </div>
+                        </article>
+                    </main>
+
+                    <script>
+                        let episode;
+                        let aktuelepisode = <?php echo get_the_ID() ?>;
+                        console.log(aktuelepisode);
+
+                        const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/epi/" + aktuelepisode;
+
+                        async function getJSON() {
+                            const data = await fetch(dbUrl);
+                            episode = await data.json();
+
+
+                            visEpi();
+                        }
+
+                        function visEpi() {
+                            document.querySelector(".billede").src = episode.billede.guid;
+                            document.querySelector(".episodenavn").textContent = episode.title.rendered;
+                            document.querySelector(".episodenr").innerHTML = episode.episodenr;
+                            document.querySelector(".om_episoden").innerHTML = episode.om_episoden;
+
+                            /*    let temp = document.querySelector("template");
+                                episode.forEach(epi => {
+                                    console.log("loop id:", aktuelepisode);
+                                    if (epi.horer_til_podcast[0].ID == aktuelepisode) {
+                                        console.log("loop kører id:", aktuelepisode);
+                                        let klon = temp.cloneNode(true).content;
+
+                                        klon.querySelector(".billede").src = epi.billede.guid;
+                                        klon.querySelector(".episodenavn").textContent = epi.title.rendered;
+                                        klon.querySelector(".episodenr").innerHTML = epi.episodenr;
+                                        klon.querySelector(".om_episoden").innerHTML = epi.om_episoden;
+
+                                    }
+                                    container.appendChild(klon);
+                                    console.log("epi", epi.link);
+                                })*/
+                        }
+                        getJSON();
+
+                    </script>
+                </section>
+                <div class="footer-wrapper">
+                    <?php get_footer(); ?>
+                </div>
--- loud-theme/single-pod.php
+++ loud-theme/single-pod.php
@@ -1,4 +1,5 @@
-<?php
+<div id="enkeltpodcast">
+    <?php
 /**
  * The template for displaying all single posts and attachments.
  *
@@ -11,95 +12,96 @@ get_header();
 do_action( 'hestia_before_single_post_wrapper' );
 ?>
 
-<div class="<?php echo hestia_layout(); ?>">
-    <div class="blog-post blog-post-wrapper">
-        <div class="container">
+    <div class="<?php echo hestia_layout(); ?>">
+        <div class="blog-post blog-post-wrapper">
+            <div class="container">
 
-            <section id="primary" class="content-area">
-                <main id="main" class="site-main">
-                    <article>
-                        <img src="" alt="" class="billede">
-                        <div>
-                            <h2 class="navn"></h2>
-                            <p class="langtekst"></p>
-                        </div>
-                    </article>
+                <section id="primary" class="content-area">
+                    <main id="main" class="site-main">
+                        <article>
+                            <img src="" alt="" class="billede">
+                            <div>
+                                <h2 class="navn"></h2>
+                                <p class="langtekst"></p>
+                            </div>
+                        </article>
 
-                    <section id="episoder">
-                        <template>
-                            <article>
-                                <img src="" alt="" class="episodebillede">
-                                <div>
-                                    <h2 class="episodenavn"></h2>
-                                    <h3 class="episodenr"></h3>
-                                    <p class="om_episoden"></p>
-                                </div>
-                            </article>
-                        </template>
-                    </section>
-                </main>
-
-
+                        <section id="episoder">
+                            <template>
+                                <article>
+                                    <img src="" alt="" class="episodebillede">
+                                    <div>
+                                        <h2 class="episodenavn"></h2>
+                                        <h3 class="episodenr"></h3>
+                                        <p class="om_episoden"></p>
+                                    </div>
+                                </article>
+                            </template>
+                        </section>
+                    </main>
 
 
-                <script>
-                    let pod;
-                    let episoder;
-                    let aktuelpodcast = <?php echo get_the_ID() ?>;
-                    const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/pod/" + aktuelpodcast;
-                    const episodeUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/epi?per_page=100";
-
-                    const container = document.querySelector("#episoder");
-
-                    async function getJSON() {
-                        const data = await fetch(dbUrl);
-                        pod = await data.json();
-
-                        const data2 = await fetch(episodeUrl);
-                        episoder = await data2.json();
-                        console.log("episoder:", episoder);
-
-                        visPod();
-                        visEpisodere();
-                    }
-
-                    function visPod() {
-                        console.log(pod.billede.guid);
-                        document.querySelector(".navn").textContent = pod.title.rendered;
-                        document.querySelector(".billede").src = pod.billede.guid;
-                        document.querySelector(".langtekst").textContent = pod.langtekst;
-                    }
-
-                    function visEpisodere() {
-                        console.log("visEpisodere");
-                        let temp = document.querySelector("template");
-                        episoder.forEach(epi => {
-                            console.log("loop id:", aktuelpodcast);
-                            if (epi.horer_til_podcast[0].ID == aktuelpodcast) {
-                                console.log("loop kører id:", aktuelpodcast);
-                                let klon = temp.cloneNode(true).content;
 
 
-                                klon.querySelector(".episodebillede").src = epi.billede.guid;
-                                klon.querySelector(".episodenavn").textContent = epi.title.rendered;
-                                klon.querySelector(".episodenr").innerHTML = epi.episodenr;
-                                klon.querySelector(".om_episoden").innerHTML = epi.om_episoden;
-                                klon.querySelector("article").addEventListener("click", () => {
-                                    location.href = epi.link;
-                                })
+                    <script>
+                        let pod;
+                        let episoder;
+                        let aktuelpodcast = <?php echo get_the_ID() ?>;
+                        const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/pod/" + aktuelpodcast;
+                        const episodeUrl = "http://helenajakobsen.com/02_kea/02_semester/tema9/radio_loud/loud/wordpress/wp-json/wp/v2/epi?per_page=100";
 
-                                klon.querySelector("article").href = epi.link;
-                                console.log("epi", epi.link);
-                                container.appendChild(klon);
-                            }
-                        })
-                    }
+                        const container = document.querySelector("#episoder");
 
-                    getJSON();
+                        async function getJSON() {
+                            const data = await fetch(dbUrl);
+                            pod = await data.json();
 
-                </script>
-            </section>
+                            const data2 = await fetch(episodeUrl);
+                            episoder = await data2.json();
+                            console.log("episoder:", episoder);
+
+                            visPod();
+                            visEpisodere();
+                        }
+
+                        function visPod() {
+                            console.log(pod.billede.guid);
+                            document.querySelector(".navn").textContent = pod.title.rendered;
+                            document.querySelector(".billede").src = pod.billede.guid;
+                            document.querySelector(".langtekst").textContent = pod.langtekst;
+                        }
+
+                        function visEpisodere() {
+                            console.log("visEpisodere");
+                            let temp = document.querySelector("template");
+                            episoder.forEach(epi => {
+                                console.log("loop id:", aktuelpodcast);
+                                if (epi.horer_til_podcast[0].ID == aktuelpodcast) {
+                                    console.log("loop kører id:", aktuelpodcast);
+                                    let klon = temp.cloneNode(true).content;
 
 
-            <div class="footer-wrapper">
-                <?php get_footer(); ?>
+                                    klon.querySelector(".episodebillede").src = epi.billede.guid;
+                                    klon.querySelector(".episodenavn").textContent = epi.title.rendered;
+                                    klon.querySelector(".episodenr").innerHTML = epi.episodenr;
+                                    klon.querySelector(".om_episoden").innerHTML = epi.om_episoden;
+                                    klon.querySelector("article").addEventListener("click", () => {
+                                        location.href = epi.link;
+                                    })
+
+                                    klon.querySelector("article").href = epi.link;
+                                    console.log("epi", epi.link);
+                                    container.appendChild(klon);
+                                }
+                            })
+                        }
+
+                        getJSON();
+
+                    </script>
+                </section>
+
+
+                <div class="footer-wrapper">
+                    <?php get_footer(); ?>
+                </div>
